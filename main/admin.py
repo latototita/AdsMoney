214ecce1cea5e804cdc5d45cbd7d673ade535cc9
latototita <85679433+latototita@users.noreply.github.com>
2023-01-15 17:31:17 +0300

    Update admin.py

--- main/admin.py
+++ main/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import *
 admin.site.register(AdsLink)
 admin.site.register(Wallet)
+admin.site.register(Websitelinks)
 admin.site.register(Withdraw)
 admin.site.register(Referred)
 admin.site.register(ReferralBonu)
