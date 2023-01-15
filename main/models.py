200c85169dd11cd5477b0efbb2d522c5961d0f89
latototita <85679433+latototita@users.noreply.github.com>
2023-01-15 17:49:35 +0300

    Update models.py

--- main/models.py
+++ main/models.py
@@ -11,6 +11,12 @@ class Websitelinks(models.Model):
 	name =models.CharField(max_length=100)
 	def __str__(self):
 		return self.name
+class WebAdsContainer(models.Model):
+	name =models.CharField(max_length=100)
+	def __str__(self):
+		return self.name
+
+
 class Wallet(models.Model):
 	name =models.CharField(max_length=100)
 	def __str__(self):
