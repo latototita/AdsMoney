f0f6e6fe92f056122b2dc1fc7d16d87a70bd2ec3
latototita <pearlmartbusinesses@gmail.com>
2023-01-17 08:10:17 -0600

    first commit

--- adschlarship/settings.py
+++ adschlarship/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-(pva#(6^ph)j9hz4w*+=h)_##$%o#ql44bdqu+khtlo++g^vp!'
 
-
+'''
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = True'''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -147,6 +147,8 @@ EMAIL_HOST='smtp.mail.yahoo.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 
+CSP_DEFAULT_SRC=("'self'",'*')
+CSP_FRAME_ANCESTORS=("'self'",'example.com')
 
 
 EMAIL_HOST_USER='pearlmartbusinesses@yahoo.com'
